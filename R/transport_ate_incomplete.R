499eef8d63487704df7ba4aed232c8c9af90b855
nt-williams <ntwilliams.personal@gmail.com>
2022-08-17 10:47:48 -0700

    updates.

--- R/transport_ate_incomplete.R
+++ R/transport_ate_incomplete.R
@@ -13,28 +13,28 @@
 transport_ate_incomplete <- function(transport_Npsem, learners, family, ...) {
     # P(S | V)
     fit_S <- regress(transport_Npsem$var("V"), transport_Npsem$var("S"), learners, "binomial", 10)
-    pred_S <- regress_predict(fit_S, transport_Npsem$data)
+    pred_S <- regress_predict(fit_S, transport_Npsem$var("V"))
 
     # P(Z | S, W)
     fit_Z <- regress(transport_Npsem$history("Z"), transport_Npsem$var("Z"), learners, "binomial", 10)
-    pred_Z <- regress_predict(fit_Z, transport_Npsem$modify("S", 1))
+    pred_Z <- regress_predict(fit_Z, transport_Npsem$modify("S", 1)$history("Z"))
 
     # E(Y| S=1, Z, W)
     fit_Y <- regress(transport_Npsem$history("Y")[transport_Npsem$var("S") == 1, ],
                      transport_Npsem$var("Y")[transport_Npsem$var("S") == 1],
                      learners, family, 10)
-    pred_Y_z <- regress_predict(fit_Y, transport_Npsem$data)
-    pred_Y_1 <- regress_predict(fit_Y, transport_Npsem$modify("Z", 1))
-    pred_Y_0 <- regress_predict(fit_Y, transport_Npsem$modify("Z", 0))
+    pred_Y_z <- regress_predict(fit_Y, transport_Npsem$history("Y"))
+    pred_Y_1 <- regress_predict(fit_Y, transport_Npsem$modify("Z", 1)$history("Y"))
+    pred_Y_0 <- regress_predict(fit_Y, transport_Npsem$modify("Z", 0)$history("Y"))
 
     # E(E(Y| S=1, Z=z, W) | S=1, V)
     fit_Pseudo_1 <- regress(transport_Npsem$data[, c(transport_Npsem$S, transport_Npsem$V)],
                             pred_Y_1, learners, "continuous", 10)
-    pred_Pseudo_1 <- regress_predict(fit_Pseudo_1, transport_Npsem$modify("S", 1))
+    pred_Pseudo_1 <- regress_predict(fit_Pseudo_1, transport_Npsem$modify("S", 1)$data[, c(transport_Npsem$S, transport_Npsem$V)])
 
     fit_Pseudo_0 <- regress(transport_Npsem$data[, c(transport_Npsem$S, transport_Npsem$V)],
                             pred_Y_0, learners, "continuous", 10)
-    pred_Pseudo_0 <- regress_predict(fit_Pseudo_0, transport_Npsem$modify("S", 1))
+    pred_Pseudo_0 <- regress_predict(fit_Pseudo_0, transport_Npsem$modify("S", 1)$data[, c(transport_Npsem$S, transport_Npsem$V)])
 
     # Influence function
     eif_1 <- transport_ate_incomplete_eif(
---
+++ tests/testthat/test-transport_ate_incomplete.R
@@ -0,0 +1,31 @@
+sim_data <- function(n) {
+    S <- rbinom(n, 1, 0.5)
+    W1 <- rbinom(n, 1, 0.4 + (0.2 * S))
+    W2 <- rnorm(n, 0.1*S, 1)
+    W3 <- rnorm(n, 1 + (0.2*S), 1)
+    # A <- rbinom(n, 1, 0.5)
+    # Z0 <- rbinom(n , 1, plogis( -log(1.6) - log(1.1)*W2 -log(1.3)*W3))
+    A <- rbinom(n, 1, plogis(-log(1.6) +log(4) - log(1.1)*W2  -log(1.3)*W3))
+    # Z <- ifelse(A == 1, Z1, Z0)
+    Y0 <- rbinom(n, 1, plogis(log(1.6) - log(1.3)*W3 - log(1.2)*W1))
+    Y1 <- rbinom(n, 1, plogis(log(1.6) + log(1.9) -log(1.3)*W3 - log(1.2)*W1 + log(1.2)*W1))
+    Y <- ifelse(A == 1, Y1, Y0)
+    list(obs = data.frame(W1 = W1, W2 = W2, W3 = W3, S = S, A = A, Y = Y, Y1 = Y1, Y0 = Y0),
+         ate = mean((Y1 - Y0)),
+         ate_S0 = mean((Y1 - Y0)[S == 0]),
+         ate_S1 = mean((Y1 - Y0)[S == 1]))
+}
+
+devtools::load_all()
+library(lmtp)
+
+set.seed(566)
+dat <- sim_data(1000)
+obs <- dat$obs
+
+Np <- transport_Npsem$new(dat$obs, c("W1", "W2", "W3"),
+                          V = c("W1", "W2", "W3"), A = NULL,
+                          Z = "A", S = "S", Y = "Y")
+
+test <- transport_ate_incomplete(Np, c("SL.glm.interaction"), "binomial")
+mean(test$eif_1 - test$eif_0)
