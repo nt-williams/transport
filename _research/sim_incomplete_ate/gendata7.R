e5a616ddb6462a3ce33ff4e0d7f30f30585612e9
nt-williams <ntwilliams.personal@gmail.com>
2023-01-17 14:38:43 -0800

    DGPs to investigate.

---
+++ _research/sim_incomplete_ate/gendata7.R
@@ -0,0 +1,54 @@
+# Results in lambda variance larger than theta variance
+gendata7 <- function(n, A = NULL) {
+    W1 <- rbinom(n, 1, 0.5)
+
+    if (is.null(A)) A <- rbinom(n, 1, 0.5)
+
+    S <- rbinom(n, 1, 0.8 - 0.7*W1)
+
+    Yi <- rnorm(n, A + W1, sqrt((0.1 + 0.8*W1)^2))
+    Y <- ifelse(S == 1, Yi, NA_real_)
+
+    data.frame(W1 = W1,
+               S = S,
+               A = A,
+               Y = Y,
+               Yi = Yi)
+}
+
+mean(subset(gendata7(1e7, 1), S == 0)$Yi) -
+    mean(subset(gendata7(1e7, 0), S == 0)$Yi)
+
+mean(subset(gendata8(1e7, 1), S == 1)$Yi) -
+    mean(subset(gendata8(1e7, 0), S == 1)$Yi)
+
+res <- map(1:500, function(x) {
+    dat <- gendata7(5000)
+    out <- vector("list", 2)
+    names(out) <- c("lambda", "theta")
+
+    Np <- transport_Npsem$new(dat, c("W1"), Z = "A", S = "S", Y = "Y")
+    out[["lambda"]] <- transport_ate(Np, c("SL.glm", "SL.mean"), "gaussian")
+
+    Np <- transport_Npsem$new(dat, c("W1"), V = NULL, Z = "A", S = "S", Y = "Y")
+    out[["theta"]] <- transport_ate_incomplete(Np, c("SL.glm", "SL.mean"), "gaussian")
+    out
+})
+
+hist(map_dbl(res, \(x) x$lambda$theta))
+hist(map_dbl(res, \(x) x$theta$theta))
+
+var(map_dbl(res, \(x) x$lambda$theta)) * 5000
+var(map_dbl(res, \(x) x$theta$theta)) * 5000
+
+mean(map_dbl(res, \(x) x$lambda$var))
+mean(map_dbl(res, \(x) x$theta$var))
+
+covered <- function(x, n) {
+    se <- sqrt(x$var) / sqrt(n)
+    ci <- x$theta + c(-1, 1)*qnorm(0.975)*se
+    dplyr::between(0.99, ci[1], ci[2])
+}
+
+mean(map_lgl(res, \(x) covered(x$lambda, n = 5000)))
+mean(map_lgl(res, \(x) covered(x$theta, n = 5000)))
---
+++ _research/sim_incomplete_ate/gendata8.R
@@ -0,0 +1,68 @@
+# Results in lambda variance larger than theta variance with a 'sufficient' set and positive correlation
+suppressPackageStartupMessages(library(tidyverse))
+
+gendata8 <- function(n, A = NULL) {
+    W1 <- rbinom(n, 1, 0.5)
+    W2 <- rbinom(n, 1, 0.75)
+
+    if (is.null(A)) {
+        A <- rbinom(n, 1, 0.5)
+    }
+
+    S <- rbinom(n, 1, 0.2 + 0.7*W1 - 0.1*W2)
+
+
+    Yi <- rnorm(n, 1.2 + 0.25*A + 0.5*W1 + A*W1 + 0.5*W2, sqrt((0.1 + 0.8*W1)^2))
+    Y <- ifelse(S == 1, Yi, NA_real_)
+
+    data.frame(W1 = W1,
+               W2 = W2,
+               S = S,
+               A = A,
+               Y = Y,
+               Yi = Yi)
+}
+
+mean(subset(gendata8(1e7, 1), S == 0)$Yi) -
+    mean(subset(gendata8(1e7, 0), S == 0)$Yi)
+
+mean(subset(gendata8(1e7, 1), S == 1)$Yi) -
+    mean(subset(gendata8(1e7, 0), S == 1)$Yi)
+
+res <- map(1:500, function(x) {
+    dat <- gendata8(5000)
+    out <- vector("list", 2)
+    names(out) <- c("lambda", "theta")
+
+    Np <- transport_Npsem$new(dat, c("W1", "W2"), Z = "A", S = "S", Y = "Y")
+    out[["lambda"]] <-
+        transport_ate(Np, c("SL.glm", "SL.glm.interaction", "SL.mean"), "gaussian")
+
+    Np <- transport_Npsem$new(dat, c("W1", "W2"), V = c("W1"), Z = "A", S = "S", Y = "Y")
+    out[["theta"]] <-
+        transport_ate_incomplete(Np, c("SL.glm", "SL.glm.interaction", "SL.mean"), "gaussian")
+
+    out
+})
+
+mean(map_dbl(res, \(x) x$lambda$theta))
+mean(map_dbl(res, \(x) x$theta$theta))
+
+hist(map_dbl(res, \(x) x$lambda$theta))
+hist(map_dbl(res, \(x) x$theta$theta))
+
+var(map_dbl(res, \(x) x$lambda$theta)) * 5000
+var(map_dbl(res, \(x) x$theta$theta)) * 5000
+
+median(map_dbl(res, \(x) x$lambda$var))
+median(map_dbl(res, \(x) x$theta$var))
+
+covered <- function(x, n) {
+    se <- sqrt(x$var) / sqrt(n)
+    ci <- x$theta + c(-1, 1)*qnorm(0.975)*se
+    dplyr::between(0.416, ci[1], ci[2])
+}
+
+mean(map_lgl(res, \(x) covered(x$lambda, n = 5000)))
+mean(map_lgl(res, \(x) covered(x$theta, n = 5000)))
+
