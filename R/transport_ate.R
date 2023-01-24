9a67fcf770b5429292cea1a12ee40bb368a4b5cc
nt-williams <ntwilliams.personal@gmail.com>
2023-01-24 14:06:46 -0800

    IPW and bug fixes to EICs

--- R/transport_ate.R
+++ R/transport_ate.R
@@ -15,9 +15,7 @@ transport_ate <- function(transport_Npsem, learner, family) {
                    learner,
                    10)
 
-    pred_Z <- predict_from_fit(fit_Z,
-                               transport_Npsem$modify("S", 1)$
-                                   history("Z", data = TRUE))
+    pred_Z <- predict_from_fit(fit_Z, transport_Npsem$modify("S", 1)$history("Z", data = TRUE))
 
     s <- transport_Npsem$var("S", data = TRUE, drop = TRUE)
 
@@ -28,27 +26,37 @@ transport_ate <- function(transport_Npsem, learner, family) {
                    learner,
                    10)
 
-    pred_Y_z <- predict_from_fit(fit_Y, transport_Npsem$
-                                     history("Y", data = TRUE))
-    pred_Y_1 <- predict_from_fit(fit_Y,
-                                 transport_Npsem$modify("Z", 1)$
-                                     history("Y", data = TRUE))
-    pred_Y_0 <- predict_from_fit(fit_Y,
-                                 transport_Npsem$modify("Z", 0)$
-                                     history("Y", data = TRUE))
+    pred_Y_z <- predict_from_fit(fit_Y, transport_Npsem$history("Y", data = TRUE))
+    pred_Y_1 <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 1)$history("Y", data = TRUE))
+    pred_Y_0 <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 0)$history("Y", data = TRUE))
 
     lambda <- mean((pred_Y_1 - pred_Y_0)[s == 0])
 
     a <- transport_Npsem$var("Z", data = TRUE, drop = TRUE)
     y <- transport_Npsem$var("Y", data = TRUE, drop = TRUE)
+    y[is.na(y)] <- -999
     pred_Z_z <- a * pred_Z + (1 - pred_Z) * (1 - a)
 
-    eic <- ifelse(s == 1,
-           (((2*a - 1) * s) / pred_Z_z) * ((1 - pred_S) / (pred_S * mean(s))) *
-               (y - pred_Y_z),
-           0) +
-        ((1 - pred_S) / mean(s)) * (pred_Y_1 - pred_Y_0 - lambda)
+    ipwz <- s / (1 - mean(s)) * (a / pred_Z - (1 - a) / (1 - pred_Z))
+    hs <- (1 - pred_S) / pred_S
 
-    list(theta = lambda + mean(eic),
-         var = var(eic))
+    eic <- ipwz * hs * (y - pred_Y_z) + ((1 - s) / (1 - mean(s))) * (pred_Y_1 - pred_Y_0 - lambda)
+
+    ipw <- s / (1 - mean(s)) * (1 / pred_Z) * hs
+    d.w <- survey::svydesign(~ 1, weights = ipw[s == 1], data = transport_Npsem$data[s == 1, ])
+    f <- reformulate(transport_Npsem$Z, transport_Npsem$Y)
+    fit <- survey::svyglm(f, design = d.w, data = transport_Npsem$data[s == 1, ])
+
+    theta <- lambda + mean(eic)
+    var <- var(eic)
+
+    se <- sqrt(var) / sqrt(nrow(transport_Npsem$data))
+    ci <- theta + c(-1, 1)*qnorm(0.975)*se
+
+    list(theta = theta,
+         var = var,
+         confint = ci,
+         ipw = as.numeric(coef(fit)[2]),
+         ipw_var = summary(fit)$coefficients[2, 2]^2 * nrow(transport_Npsem$data),
+         ipw_confint = as.numeric(confint(fit)[2, ]))
 }
--- R/transport_ate_incomplete.R
+++ R/transport_ate_incomplete.R
@@ -20,9 +20,7 @@ transport_ate_incomplete <- function(transport_Npsem, learner, family) {
                    learner,
                    10)
 
-    pred_Z <- predict_from_fit(fit_Z,
-                               transport_Npsem$modify("S", 1)$
-                                   history("Z", data = TRUE))
+    pred_Z <- predict_from_fit(fit_Z, transport_Npsem$modify("S", 1)$history("Z", data = TRUE))
 
     s <- transport_Npsem$var("S", data = TRUE, drop = TRUE)
 
@@ -33,17 +31,13 @@ transport_ate_incomplete <- function(transport_Npsem, learner, family) {
                    learner,
                    10)
 
-    pred_Y_z <- predict_from_fit(fit_Y, transport_Npsem$
-                                     history("Y", data = TRUE))
-    pred_Y_1 <- predict_from_fit(fit_Y,
-                                 transport_Npsem$modify("Z", 1)$
-                                     history("Y", data = TRUE))
-    pred_Y_0 <- predict_from_fit(fit_Y,
-                                 transport_Npsem$modify("Z", 0)$
-                                     history("Y", data = TRUE))
+    pred_Y_z <- predict_from_fit(fit_Y, transport_Npsem$history("Y", data = TRUE))
+    pred_Y_1 <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 1)$history("Y", data = TRUE))
+    pred_Y_0 <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 0)$history("Y", data = TRUE))
 
     a <- transport_Npsem$var("Z", data = TRUE, drop = TRUE)
     y <- transport_Npsem$var("Y", data = TRUE, drop = TRUE)
+    y[is.na(y)] <- -999
     pred_Z_z <- a * pred_Z + (1 - pred_Z) * (1 - a)
 
     # construct T_(O, P)
@@ -60,12 +54,26 @@ transport_ate_incomplete <- function(transport_Npsem, learner, family) {
 
     theta_init <- mean(f_V[s == 0])
 
-    eic <- ifelse(s == 1,
-                  ((s * (2*a - 1)) / pred_Z_z) * ((1 - pred_S) / (pred_S * mean(s))) *
-                      (y - a*f_V - pred_Y_0),
-                  0) +
-        (((1 - pred_S) / mean(s)) * (f_V - theta_init))
+    ipwz <- s / (1 - mean(s)) * (a / pred_Z - (1 - a) / (1 - pred_Z))
+    hs <- (1 - pred_S) / pred_S
 
-    list(theta = theta_init + mean(eic),
-         var = var(eic))
+    eic <- ipwz * hs  * (y - a*f_V - pred_Y_0) + ((1 - s) / (1 - mean(s))) * (f_V - theta_init)
+
+    ipw <- s / (1 - mean(s)) * (1 / pred_Z) * hs
+    d.w <- survey::svydesign(~ 1, weights = ipw[s == 1], data = transport_Npsem$data[s == 1, ])
+    f <- reformulate(transport_Npsem$Z, transport_Npsem$Y)
+    fit <- survey::svyglm(f, design = d.w, data = transport_Npsem$data[s == 1, ])
+
+    theta <- theta_init + mean(eic)
+    var <- var(eic)
+
+    se <- sqrt(var) / sqrt(nrow(transport_Npsem$data))
+    ci <- theta + c(-1, 1)*qnorm(0.975)*se
+
+    list(theta = theta,
+         var = var,
+         confint = ci,
+         ipw = as.numeric(coef(fit)[2]),
+         ipw_var = summary(fit)$coefficients[2, 2]^2 * nrow(transport_Npsem$data),
+         ipw_confint = as.numeric(confint(fit)[2, ]))
 }
