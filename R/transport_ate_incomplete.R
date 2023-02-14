2fca8bb5bd37b68acd603c3b99cc31c710e6c7ed
nt-williams <ntwilliams.personal@gmail.com>
2023-02-14 08:53:58 -0800

    transport_ate_incomplete wioth crossfitting.

--- R/transport_ate_incomplete.R
+++ R/transport_ate_incomplete.R
@@ -1,56 +1,74 @@
-transport_ate_incomplete <- function(transport_Npsem, learner, family) {
-    v <- transport_Npsem$var("V", data = TRUE)
-    if (ncol(v) == 0) {
-        v <- data.frame(X = rep(1, nrow(transport_Npsem$data)))
+transport_ate_incomplete <- function(transport_Npsem, learner, family, folds = 20) {
+    folded <- make_folds(transport_Npsem$data, folds)
+
+    V <- transport_Npsem$var("V", data = TRUE)
+    if (ncol(V) == 0) {
+        V <- data.frame(X = rep(1, nrow(transport_Npsem$data)))
     }
 
-    # compute P(S | V)
-    fit_S <- train(v,
-                   transport_Npsem$var("S", data = TRUE, drop = TRUE),
-                   "binomial",
-                   learner,
-                   10)
-
-    pred_S <- predict_from_fit(fit_S, v)
-
-    # compute P(Z | S, W)
-    fit_Z <- train(transport_Npsem$history("Z", data = TRUE),
-                   transport_Npsem$var("Z", data = TRUE, drop = TRUE),
-                   "binomial",
-                   learner,
-                   10)
-
-    pred_Z <- predict_from_fit(fit_Z, transport_Npsem$modify("S", 1)$history("Z", data = TRUE))
-
     s <- transport_Npsem$var("S", data = TRUE, drop = TRUE)
-
-    # compute E(Y| S=1, Z, W)
-    fit_Y <- train(transport_Npsem$history("Y", data = TRUE)[s == 1, ],
-                   transport_Npsem$var("Y", data = TRUE, drop = TRUE)[s == 1],
-                   family,
-                   learner,
-                   10)
-
-    pred_Y_z <- predict_from_fit(fit_Y, transport_Npsem$history("Y", data = TRUE))
-    pred_Y_1 <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 1)$history("Y", data = TRUE))
-    pred_Y_0 <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 0)$history("Y", data = TRUE))
-
     a <- transport_Npsem$var("Z", data = TRUE, drop = TRUE)
     y <- transport_Npsem$var("Y", data = TRUE, drop = TRUE)
     y[is.na(y)] <- -999
-    pred_Z_z <- a * pred_Z + (1 - pred_Z) * (1 - a)
 
-    # construct T_(O, P)
-    tmp_T_OP <- ((2*a - 1) / pred_Z_z) * (y - pred_Y_z) + pred_Y_1 - pred_Y_0
+    pred_S <- vector("numeric", nrow(transport_Npsem$data))
+    pred_Z <- vector("numeric", nrow(transport_Npsem$data))
+    pred_Y_z <- vector("numeric", nrow(transport_Npsem$data))
+    pred_Y_1 <- vector("numeric", nrow(transport_Npsem$data))
+    pred_Y_0 <- vector("numeric", nrow(transport_Npsem$data))
+    f_V <- vector("numeric", nrow(transport_Npsem$data))
+    for (i in 1:folds) {
+        t <- folded[[i]]$training_set
+        v <- folded[[i]]$validation_set
 
-    # compute f(V)
-    fit_V <- train(v[s == 1, , drop = FALSE],
-                   tmp_T_OP[s == 1],
-                   "gaussian",
-                   learner,
-                   10)
+        # compute P(S | V)
+        fit_S <- train(V[t, , drop = FALSE],
+                       transport_Npsem$var("S", data = TRUE, drop = TRUE)[t],
+                       "binomial",
+                       learner,
+                       10)
 
-    f_V <- predict_from_fit(fit_V, v)
+        pred_S[v] <- predict_from_fit(fit_S, V[v, , drop = FALSE])
+
+        # compute P(Z | S, W)
+        fit_Z <- train(transport_Npsem$history("Z", data = TRUE)[t, ],
+                       transport_Npsem$var("Z", data = TRUE, drop = TRUE)[t],
+                       "binomial",
+                       learner,
+                       10)
+
+        pred_Zt <- predict_from_fit(fit_Z, transport_Npsem$modify("S", 1)$history("Z", data = TRUE)[t, ])
+        pred_Z[v] <- predict_from_fit(fit_Z, transport_Npsem$modify("S", 1)$history("Z", data = TRUE)[v, ])
+
+        # compute E(Y| S=1, Z, W)
+        fit_Y <- train(transport_Npsem$history("Y", data = TRUE)[t, ][s[t] == 1, ],
+                       transport_Npsem$var("Y", data = TRUE, drop = TRUE)[t][s[t] == 1],
+                       family,
+                       learner,
+                       10)
+
+        pred_Y_zt <- predict_from_fit(fit_Y, transport_Npsem$history("Y", data = TRUE)[t, ])
+        pred_Y_1t <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 1)$history("Y", data = TRUE)[t, ])
+        pred_Y_0t <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 0)$history("Y", data = TRUE)[t, ])
+
+        pred_Y_z[v] <- predict_from_fit(fit_Y, transport_Npsem$history("Y", data = TRUE)[v, ])
+        pred_Y_1[v] <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 1)$history("Y", data = TRUE)[v, ])
+        pred_Y_0[v] <- predict_from_fit(fit_Y, transport_Npsem$modify("Z", 0)$history("Y", data = TRUE)[v, ])
+
+        pred_Z_z <- a[t] * pred_Zt + (1 - pred_Zt) * (1 - a[t])
+
+        # construct T_(O, P)
+        tmp_T_OP <- ((2*a[t] - 1) / pred_Z_z) * (y[t] - pred_Y_zt) + pred_Y_1t - pred_Y_0t
+
+        # compute f(V)
+        fit_V <- train(V[t, , drop = FALSE][s[t] == 1, , drop = FALSE],
+                       tmp_T_OP[s[t] == 1],
+                       "gaussian",
+                       learner,
+                       10)
+
+        f_V[v] <- predict_from_fit(fit_V, V[v, , drop = FALSE])
+    }
 
     theta_init <- mean(f_V[s == 0])
 
