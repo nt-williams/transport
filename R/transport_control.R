338d653a83229b394695f281da71f6d4f1c23273
Nicholas Williams <ntwilliams.personal@gmail.com>
2024-10-31 14:56:46 -0700

    Basic documentation for transport_control

--- R/transport_control.R
+++ R/transport_control.R
@@ -1,15 +1,24 @@
-#' Title
+#' Set Estimation Parameters
 #'
-#' @param .learners_folds
-#' @param .bound
-#' @param .return_full_fits
-#' @param .discrete
-#' @param .info
+#' Defines default parameters for estimators in the 'transport' package.
 #'
-#' @return
+#' @param .learners_folds \[\code{integer(1)}\]\cr
+#'  The number of cross-validation folds for fitting nuisance parameters.
+#' @param .bound \[\code{numeric(1)}\]\cr
+#'  Determines that maximum and minimum values binomial predictions
+#'  will be bounded by. The default is 1e-5, bounding predictions by 1e-5 and 0.9999.
+#' @param .return_full_fits \[\code{logical(1)}\]\cr
+#'  Return full 'mlr3superlearner' fits? Default is \code{FALSE}, return only 'mlr3superlearner' weights.
+#' @param .discrete \[\code{logical(1)}\]\cr
+#'  Use discrete or ensemble super learner? Default is \code{FALSE}.
+#' @param .info \[\code{logical(1)}\]\cr
+#'  Print super learner fitting info to the console? Default is \code{FALSE}.
+#'
+#' @return A list with parameter values.
 #' @export
 #'
 #' @examples
+#' transport_control(.learners_folds = 10)
 transport_control <- function(.learners_folds = NULL,
                               .bound = 1e5,
                               .return_full_fits = FALSE,
